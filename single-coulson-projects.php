49540da296cf70ee33671f4b602f20a77678159d
Chris Coulson <chrisjamescoulson@gmail.com>
2021-10-27 17:53:27 -0700

    Began to fill in ACF content for single projects page

--- single-coulson-projects.php
+++ single-coulson-projects.php
@@ -15,20 +15,89 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'coulson' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'coulson' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
+		if ( get_field( 'project_title' ) ) :
+			?>
+			<h1 class='project-title'> <?php the_field( 'project_title' ); ?> </h1>
+				<?php
 			endif;
+
+			$featured_img = get_field( 'project_featured_image' );
+		if ( ! empty( $featured_img ) ) :
+			?>
+				<img src="<?php echo esc_url( $featured_img['url'] ); ?>" alt="<?php echo esc_attr( $featured_img['alt'] ); ?>" />
+				<?php
+			endif;
+
+		if ( get_field( 'project_description' ) ) :
+			?>
+				<p><?php the_field( 'project_description' ); ?></p>
+				<?php
+			endif;
+		?>
+
+		<h2>Planning</h2>
+		<?php
+		$planning = get_field( 'planning_group' );
+		if ( $planning ) :
+			?>
+			<section id='planning'>
+				<p id="planning-intro"> <?php echo esc_html( $planning['planning_text'] ); ?> </p>
+
+				<img src="<?php echo esc_url( $planning['planning_image']['url'] ); ?>" alt="<?php echo esc_attr( $planning['planning_image']['alt'] ); ?>" />
+
+				<div id="planning-outro"><?php echo $planning['planning_list']; ?></div>
+			</section>
+			<?php
+		endif;
+		?>
+
+		<h2>Design</h2>
+		<?php 
+		$design = get_field( 'design_group' );
+		if ( $design ) :
+		?>
+			<section id="design">
+				<img src="<?php echo esc_url( $design['design_image']['url'] ); ?>" alt="<?php echo esc_attr( $design['design_image']['alt'] ); ?>" />
+
+				<p id='design-intro'><?php echo esc_html( $design['design_text'] ); ?></p>
+
+				<img src="<?php echo esc_url( $design['design_image_2']['url'] ); ?>" alt="<?php echo esc_attr( $design['design_image_2']['alt'] ); ?>" />
+			</section>
+			<?php
+		endif;
+		?>
+
+		<h2>Development</h2>
+		<?php 
+		$development = get_field( 'development_group' );
+		if ( $development ) :
+		?>
+			<section id="development">
+				<p id='development-intro'><?php echo esc_html( $development['development_text'] ); ?></p>
+
+				<div>
+					<embed src="<?php echo esc_url( $development['development_embed']['url'] ); ?>" type="" />
+				</div>
+			</section>
+			<?php
+		endif;
+		?>
+
+		<h2>Reflections</h2>
+		<?php
+		$reflections = get_field( 'reflections_group' );
+		if ( $reflections ) :
+		?>
+			<section id="reflections">
+				<p id='reflections-intro'><?php echo esc_html( $reflections['reflections_text'] ); ?></p>
+
+				<img src="<?php echo esc_url( $reflections['reflections_image']['url'] ); ?>" alt="<?php echo esc_attr( $reflections['reflections_image']['alt'] ); ?>" />
+			</section>
+			<?php
+		endif;
+
 
 		endwhile; // End of the loop.
 		?>
